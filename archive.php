7ecc53e8f0688a41adcdf92e87b9db698738af39
Larry <tradesouthwest@outlook.com>
2018-01-02 18:22:57 -0700

    replc

--- archive.php
+++ archive.php
@@ -2,41 +2,87 @@
 
     <div id="content" class="row">
 
-	    <div id="main" class="col-xs-12 col-sm-6 col-md-7 col-lg-7" role="main">
+	    <div id="main" class="col-xs-12 col-sm-12 col-md-8 col-lg-8" role="main">
 
+		  <?php if (have_posts()) : ?>
 
-		<?php if (have_posts()) : ?>
-		<?php while (have_posts()) : the_post(); ?>
+			<header id="archive-header">
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class( 'page-lines' ); ?>>
-            <h2 class="entry-title">
-             <a class="text-dark"
-                href="<?php the_permalink(); ?>"
-                title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-            <section class="post_content">
+				<h1 class="page-title">
+					<?php if ( is_category() ) : ?>
+						<?php the_archive_title( '', false ); ?>
 
+					<?php elseif ( is_author() ) : ?>
+						<?php esc_html_e( 'Author Archive for ', 'appeal' ); ?>
+						<span class="text-note"> 
+                            <?php printf( esc_attr( get_the_author_meta( 
+                                      'display_name', get_query_var( 'author' ) ) ) 
+                                    ); ?></span>
+
+					<?php elseif ( is_tag() ) : ?>
+						<?php echo esc_html_e( 'Tag Archive for ', 'appeal' ); ?>
+						<span class="text-note"> 
+                        <?php the_archive_title( ); ?></span>
+
+					<?php elseif ( is_day() ) : ?>
+						<?php echo esc_html_e( 'Dated Archives: ', 'appeal' ); ?>
+						<span class="text-note"> 
+                            <?php printf( esc_attr( get_the_date() ) ); ?></span>
+
+					<?php elseif ( is_month() ) : ?>
+						<?php echo esc_html_e( 'Monthly Archives: ', 'appeal' ); ?>
+						<span class="text-note"> 
+                            <?php printf( esc_attr( get_the_date( 'F Y' ) ) ); ?></span>
+
+					<?php elseif ( is_year() ) : ?>
+						<?php echo esc_html_e( 'Yearly Archives: ', 'appeal' ); ?>
+						<span class="text-note"> 
+                            <?php printf( esc_attr( get_the_date( 'Y' ) ) ); ?></span>
+
+    					<?php else : ?>
+    						<?php the_archive_title(); ?>
+    						<?php the_archive_description(); ?>
+
+    				<?php endif; ?>
+				</h1><!-- .page-title -->
+				
+				<?php
+				$category_description = the_archive_description();
+				if ( $category_description != '' ) : ?> 
+				
+				    <div class="archive-meta">
+				        <p><?php the_archive_description(); ?></p>
+				    </div>
+				
+				<?php endif; ?>
+
+    		</header><!-- #archive-header -->
             <?php
-            $length          = appeal_custom_posts_excerpt_length();
-            $appealmore      = appeal_custom_excerpt_more();
-            $content         = get_the_content();
-            $trimmed_content = wp_trim_words( $content, $length, $appealmore );
+            while ( have_posts() ) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h1 class="entry-title"><?php the_archive_title(); ?> 
+                <a href="<?php the_permalink() ?>" rel="bookmark" 
+                   title="<?php the_archive_title(); ?>" >
+                   <?php the_title(); ?></a></h1>
+                <div class="entry-content">
 
-            echo '<p>'.$trimmed_content.'</p>'; ?>
+                    <?php the_excerpt(); ?>
+                    
+                         <a href="<?php echo esc_url( get_permalink() ); ?>" 
+                            title="<?php the_title_attribute(); ?>"> 
+                      <?php esc_attr_e( 'View Article...', 'appeal' ); ?></a>
+                </div><!-- ends entry-content -->   
+                    <footer class="meta-footer archive-footer">
 
+                    <?php $time_format = get_the_date( get_option('date_format') ); ?>
+                     
+                    <span class="alignright mb1" 
+                      itemprop="datePublished"><?php echo esc_html( $time_format ); ?></span> 
+                      
+                    </footer>
+            </article><!-- #post -->
 
-                    <nav class="pagination"><?php // more tag display
-                    wp_link_pages();
-                    ?></nav>
-
-            </section>
-                <footer class="meta-footer">
-
-                    <?php get_template_part('part', 'metadata' ); ?>
-
-                </footer><div class="clearfix"></div>
-
-        </article>
-		<?php endwhile; ?>
+        <?php endwhile; // end of the loop ?>
 
 		<?php else : ?>
 
@@ -47,12 +93,8 @@
                     <?php get_template_part( 'nav', 'content' ); ?>
 
 	    </div>
-        <div class="col-xs-12 col-sm-6 col-md-2 col-lg-2">
-
-            <?php get_sidebar( 'left'); ?>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+	    
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 
             <?php get_sidebar( 'right' ); ?>
 
