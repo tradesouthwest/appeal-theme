521c033b94c73eb799e005db52c966de4d8c58ba
Larry <tradesouthwest@outlook.com>
2018-01-02 18:36:56 -0700

    no change

--- nav-content.php
+++ nav-content.php
@@ -1,33 +1,46 @@
 
     <?php if (get_next_posts_link() || get_previous_posts_link()) { ?>
     <div class="postlink">
-        <nav class="block">
+        <nav class="block" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
             <ul class="list-inline">
+            
+            <?php //only show if there are more posts
+            if( get_next_posts_link() ) { ?>
+            
                 <li class="btn btn-paging notblank previous">
+            
                     <?php next_posts_link("&laquo; "
                           . __('Older posts', "appeal")); ?></li>
+            <?php } ?>
+            
                 <li></li>
-                <li class="btn btn-paging notblank"> &laquo; &nbsp; &raquo; </li>
+            
+                <?php //only show if more pagination of posts
+                if( get_previous_posts_link()) { ?>
+            
                 <li></li>
                 <li class="btn btn-paging notblank next">
+            
                     <?php previous_posts_link(
                           __('Newer posts', "appeal") . " &raquo;"); ?></li>
+                <?php } ?>
+            
             </ul>
         </nav>
     </div>
     <?php } ?>
 
 
-<?php if( is_page() || is_single() ) {
+<?php if( is_page() || is_single() ) { 
 ?>
-<div class="pagination">
+<div class="pagination" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 <?php
 the_post_navigation( array(
 	'prev_text' => '<span class="screen-reader-text">'
     . __( 'Previous Post', 'appeal' )
     . '</span><span aria-hidden="true" class="nav-subtitle">'
     . __( '&laquo; Previous: ', 'appeal' ) . '</span>
-    <span class="nav-pills">%title</span><br>',
+    <span class="nav-pills">%title</span>',
 
     'next_text' => '<span class="screen-reader-text">'
     . __( 'Next Post', 'appeal' ) . '</span>
@@ -39,4 +52,4 @@ the_post_navigation( array(
 </div>
 <?php
 }
-?> 
+?>
