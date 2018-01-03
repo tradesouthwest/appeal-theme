b1e7e3faa51bb2a34318681d7f713052ddfca54a
Larry <tradesouthwest@outlook.com>
2018-01-02 18:48:54 -0700

    v1.2.4

--- single.php
+++ single.php
@@ -2,16 +2,39 @@
 
     <div id="content" class="row">
 
+    <?php //use 9 by 3 if gallery or image post type
+    if( has_post_format( 'gallery' ) || has_post_format( 'image' ) ) { ?>
+
+        <div id="main" class="col-xs-12">
+
+            <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+
+                <?php get_template_part( 'content' ); ?>
+
+            <?php endwhile; ?>
+                <?php else : ?>
+
+                    <?php get_template_part( 'nothing' ); ?>
+
+            <?php endif; ?>
+
+            <?php get_template_part( 'nav', 'content' ); ?>
+
+        </div>
+
+        <?php } //ends gallery wider width page
+        else { ?>
+
+
 	    <div id="main" class="col-xs-12 col-sm-6 col-md-7 col-lg-7" role="main">
 
 		<?php if (have_posts()) : ?>
-
 		<?php while (have_posts()) : the_post(); ?>
 
 		    <?php get_template_part( 'content' ); ?>
 
 		<?php endwhile; ?>
-
 		<?php else : ?>
 
 		    <?php get_template_part( 'nothing' ); ?>
@@ -21,6 +44,7 @@
             <?php get_template_part( 'nav', 'content' ); ?>
 
 	    </div>
+
         <div class="col-xs-12 col-sm-6 col-md-2 col-lg-2">
 
             <?php get_sidebar( 'left'); ?>
@@ -31,7 +55,8 @@
             <?php get_sidebar( 'right' ); ?>
 
         </div>
-
+<?php } // ends display three columns if not gallery
+?>
 
     </div><!-- ends #content .row -->
 
